cf7c72799ee06b1739cef392308e48a47f8bfe36
Cyril T <34008437+Valamyrr@users.noreply.github.com>
2026-01-05 17:31:13 +0100

    Update add_character.py

--- scripts/add_character.py
+++ scripts/add_character.py
@@ -2,8 +2,10 @@ from pathlib import Path
 import os
 import re
 
-ROOT = Path(__file__).resolve().parent.parent
+ROOT = Path(__file__).resolve().parent
+print(ROOT)
 CSV_PATH = ROOT / "personnages.csv"
+print(CSV_PATH)
 
 body = os.environ["ISSUE_BODY"]
 
