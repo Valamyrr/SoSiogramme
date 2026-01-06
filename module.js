42e0fcc5e208478153ac9dd11cd94ed71419d265
Cyril T <34008437+Valamyrr@users.noreply.github.com>
2026-01-06 13:18:46 +0100

    Update module.js

--- module.js
+++ module.js
@@ -244,6 +244,7 @@ function draw(isDragging = false, skipForces = false) {
 
   const show = Array.from(document.querySelectorAll('.sidebar input[data-type]:checked')).map(c => c.dataset.type);
   const activeGroups = Array.from(document.querySelectorAll('.sidebar input[data-group]:checked')).map(c => c.dataset.group);
+  const showNPCs = document.querySelectorAll('.sidebar #toggle-pnjs:checked').length>0
   const search = document.getElementById("searchName").value.toLowerCase();
 
   let filteredIds = new Set();
@@ -269,6 +270,9 @@ function draw(isDragging = false, skipForces = false) {
       if (!a || !b) return;
       if (!activeGroups.includes(a.groupe) || !activeGroups.includes(b.groupe)) return;
       if (search && (!filteredIds.has(rel.source) || !filteredIds.has(rel.cible))) return;
+	  if (a.type == "PNJ" || b.type == "PNJ){
+		  if(!showNPCs) return;
+	  }
 
       const showAtoB = show.includes(rel.typeAversB);
       const showBtoA = show.includes(rel.typeBversA);
@@ -428,6 +432,7 @@ function draw(isDragging = false, skipForces = false) {
   personnages.forEach(p => {
     if (!activeGroups.includes(p.groupe)) return;
     if (search && !filteredIds.has(p.nom)) return;
+	if (p.type == "PNJ" && !showNPCs) return;
 
     const g = document.createElementNS(NS, "g");
     g.setAttribute("class", selectedPersonnages.has(p.nom) ? "node selected" : "node");
