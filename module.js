539cfcb5331e16d0e203487503d4c2c07d26ff0b
Cyril T <34008437+Valamyrr@users.noreply.github.com>
2026-01-09 23:58:50 +0100

    Update module.js

--- module.js
+++ module.js
@@ -721,5 +721,28 @@ document.getElementById("export").addEventListener("click",()=>{
 });
 
 document.getElementById("import").addEventListener("click",()=>{
-	
+	document.getElementById("importInput").click();
 }
+document.getElementById("importInput").addEventListener("change", (event) => {
+  const file = event.target.files[0];
+  if (!file) return;
+  const reader = new FileReader();
+  reader.onload = (e) => {
+	try {
+        const jsonContent = JSON.parse(e.target.result);
+	    console.log("Contenu importé :",jsonContent)	
+        jsonContent.forEach(pi => {
+		    personnages.forEach(p => {
+				if(pi.nom == p.nom){
+					p.x = pi.x
+					p.y = pi.y
+				}
+			});
+	    });
+    } catch (error) {
+      console.error("JSON invalide:",error);
+    }
+  };
+
+  reader.readAsText(file);
+});
